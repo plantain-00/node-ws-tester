ec33f52d313ec1b7ee464f3cb3ae86c33407b51a
york yao <yaoao12306@outlook.com>
2017-08-16 20:17:58 +0800

    improve tslint

--- src/client.ts
+++ src/client.ts
@@ -2,12 +2,12 @@ import * as bytes from "bytes";
 import { config } from "./client.config";
 import * as WebSocket from "ws";
 
-function print(message: string | Buffer) {
+function printInConsole(message: string | Buffer) {
     // tslint:disable-next-line:no-console
     console.log(message);
 }
 
-print(`Connecting to ${config.url} with ${config.connectionCount} times.`);
+printInConsole(`Connecting to ${config.url} with ${config.connectionCount} times.`);
 
 const wsArray: WebSocket[] = [];
 let errorCount = 0;
@@ -33,7 +33,7 @@ connect(config.connectionCount);
 
 let timer: NodeJS.Timer;
 if (config.connectionCountIncrease > 0 && config.increasePerSecond > 0) {
-    print(`Connection increase ${config.connectionCountIncrease} times per ${config.increasePerSecond} second.`);
+    printInConsole(`Connection increase ${config.connectionCountIncrease} times per ${config.increasePerSecond} second.`);
     timer = setInterval(() => {
         connect(config.connectionCountIncrease);
     }, 1000 * config.increasePerSecond);
@@ -44,5 +44,5 @@ setInterval(() => {
         clearInterval(timer);
     }
     const memory = bytes.format(process.memoryUsage().rss);
-    print(`errors: ${errorCount} connections: ${wsArray.length} messages: ${bytes.format(messageTotalLength)} ${messageCount} memory: ${memory}`);
+    printInConsole(`errors: ${errorCount} connections: ${wsArray.length} messages: ${bytes.format(messageTotalLength)} ${messageCount} memory: ${memory}`);
 }, 1000);
